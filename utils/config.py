3c229fd71f16442b50bd950a70ee3d86502c54a6
egarg0587 <garg.eshant@gmail.com>
2024-07-26 17:25:50 +0530

    Revert "First commit for complete project push"
    This reverts commit 366362bd57e62b297b6e28870ea06c20063f710c.

--- utils/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-API_BASE_URL = 'https://api.github.com'
-# API_TOKEN = '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'
-
-# OWNER = "ashok512512"
-OWNER='ProAcccount'
-API_TOKEN='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'
